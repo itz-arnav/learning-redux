0c4a64aeb0f7b32dd27d336464cac1fb3438ee5f
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2022-09-24 07:56:36 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import PageNotFound from './components/PageNotFound';
+import AllProducts from './components/AllProducts';
+import ProductDetail from './components/ProductDetail';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<AllProducts />} />
+          <Route exact path="/products/:productId" element={<ProductDetail />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
